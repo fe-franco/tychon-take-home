c560a5b8bea67e3a23b5d61cffb0fcbc1181efb0
Felipe <felipe.francc2@gmail.com>
2023-05-21 06:39:10 -0300

    migrate to supabase

--- apps/nextjs/src/pages/triangle.tsx
+++ apps/nextjs/src/pages/triangle.tsx
@@ -1,9 +1,58 @@
-import { createRef, useEffect, useMemo, useState } from "react";
+import {
+  createRef,
+  useEffect,
+  useMemo,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { set } from "zod";
+import { zoom as D3zoom, select, zoomIdentity, type D3ZoomEvent } from "d3";
 
 import { api } from "~/utils/api";
+
+const useZoom = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
+  useEffect(() => {
+    if (!ref.current) return;
+    const container = select<HTMLDivElement, unknown>(ref.current);
+    const child = container.select("div");
+
+    const handleZoom = (event: D3ZoomEvent<HTMLDivElement, unknown>) => {
+      const { transform } = event;
+      child.style(
+        "transform",
+        `translate(${transform.x}px, ${transform.y}px) scale(${transform.k})`,
+      );
+      child.style("transform-origin", "0 0");
+    };
+
+    const zoomBehavior = D3zoom<HTMLDivElement, unknown>()
+      .scaleExtent([0.1, 5])
+      .extent([
+        [0, 0],
+        [ref.current.clientWidth, ref.current.clientHeight],
+      ])
+      .on("zoom", handleZoom);
+
+    container.call(zoomBehavior);
+
+    // Set initial zoom level and center the canvas
+    const initialScale = 1;
+    const initialTranslate = [0, 0];
+    container.call(
+      zoomBehavior.transform,
+      zoomIdentity
+        .scale(initialScale)
+        .translate(initialTranslate[0] ?? 0, initialTranslate[1] ?? 0),
+    );
+
+    // Clean up zoom behavior on unmount
+    return () => {
+      container.on(".zoom", null);
+    };
+  }, [ref]);
+};
 
 const getNumbers = (triangle: string) => {
   const rows = triangle.split("\n");
@@ -18,8 +67,87 @@ const getNumbers = (triangle: string) => {
 };
 
 const Triangle: NextPage = () => {
-  const { mutate, data, isLoading } = api.triangle.maxValuePath.useMutation();
+  const { mutate, data } = api.triangle.maxValuePath.useMutation();
   const [triangle, setTriangle] = useState<string>("");
+  const zoomRef = createRef<HTMLDivElement>();
+
+  useZoom(zoomRef);
+
+  useEffect(() => {
+    if (data) {
+      setTriangle((prev) => {
+        const newTriangle: string[][] = [
+          ...prev.split("\n").map((row) => row.split(" ")),
+        ];
+        data?.usedCells.forEach(({ row, col }) => {
+          if (newTriangle[row])
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            newTriangle[row]![col] = `${
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              newTriangle[row]![col]?.trim().split("-")[0]
+            }-used`;
+        });
+        return newTriangle.map((row) => row.join(" ")).join("\n");
+      });
+    }
+  }, [data]);
+
+  return (
+    <>
+      <Head>
+        <title>{`Tychon Take Home | Triangle`}</title>
+        <meta name="description" content={`Tychon Take Home | Triangle`} />
+        <link rel="icon" href={`/favicon.ico`} />
+      </Head>
+      <main
+        className={`flex h-full min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white`}
+      >
+        <div
+          className={`container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8`}
+        >
+          <h1
+            className={`text-5xl font-extrabold tracking-tight sm:text-[5rem]`}
+          >
+            Triangle Max Value Path Calculation
+          </h1>
+
+          <h2 className={`text-2xl font-bold text-pink-400`}>
+            Enter the triangle
+          </h2>
+          <span>{/* {t.x} {t.y} {t.k} */}</span>
+
+          <div
+            className={`flex h-96 w-96 items-center justify-center  overflow-hidden rounded-md bg-white/10`}
+            ref={zoomRef}
+          >
+            <div className={`flex rounded-md bg-white/10 p-5`} ref={zoomRef}>
+              <div className={`flex h-max w-max flex-col items-center gap-4`}>
+                <TriangleInput triangle={triangle} setTriangle={setTriangle} />
+              </div>
+            </div>
+          </div>
+
+          <button
+            className={`rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20`}
+            onClick={() => {
+              mutate(triangle);
+            }}
+          >
+            Calculate
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+const TriangleInput = ({
+  triangle,
+  setTriangle,
+}: {
+  triangle: string;
+  setTriangle: Dispatch<SetStateAction<string>>;
+}) => {
   const numbers = useMemo(() => getNumbers(triangle), [triangle]);
 
   const updateValue = ({
@@ -28,7 +156,6 @@ const Triangle: NextPage = () => {
     col,
   }: {
     value: string;
-    used: boolean;
     row: number;
     col: number;
   }) => {
@@ -96,101 +223,42 @@ const Triangle: NextPage = () => {
     numbers?.[row]?.[col]?.ref.current?.focus();
   };
 
-  useEffect(() => {
-    if (data) {
-      setTriangle((prev) => {
-        const newTriangle: string[][] = [
-          ...prev.split("\n").map((row) => row.split(" ")),
-        ];
-        data?.usedCells.forEach(({ row, col }) => {
-          if (newTriangle[row])
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            newTriangle[row]![col] = `${
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              newTriangle[row]![col]?.trim().split("-")[0]
-            }-used`;
-        });
-        return newTriangle.map((row) => row.join(" ")).join("\n");
-      });
-    }
-  }, [data]);
-
   return (
     <>
-      <Head>
-        <title>{`Tychon Take Home | Triangle`}</title>
-        <meta name="description" content={`Tychon Take Home | Triangle`} />
-        <link rel="icon" href={`/favicon.ico`} />
-      </Head>
-      <main
-        className={`flex h-full min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white`}
-      >
-        <div
-          className={`container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8`}
-        >
-          <h1
-            className={`text-5xl font-extrabold tracking-tight sm:text-[5rem]`}
-          >
-            Triangle Max Value Path Calculation
-          </h1>
-
-          <div className={`flex flex-col gap-4`}>
-            <h2 className={`text-2xl font-bold text-pink-400`}>
-              Enter the triangle
-            </h2>
-
-            {/* {JSON.stringify(triangle)}
-            {JSON.stringify(data)} */}
-
-            <div className={`flex flex-col items-center gap-4`}>
-              {numbers.map((row, i) => (
-                <div key={i} className={`flex gap-4`}>
-                  {row.map(({ value, used, ref }, j) => (
-                    <input
-                      contentEditable={true}
-                      type="number"
-                      key={j}
-                      ref={ref}
-                      className={`resize-none overflow-hidden rounded-md ${
-                        used ? "bg-pink-400" : "bg-white/10"
-                      } h-12 w-12 text-center font-semibold text-white no-underline transition hover:bg-white/20`}
-                      value={value}
-                      onKeyDownCapture={(e) => {
-                        keyInput({
-                          value: e.key === " " ? "Space" : e.key,
-                          row: i,
-                          col: j,
-                        });
-                      }}
-                      onChange={(e) => {
-                        updateValue({
-                          value: e.target.value,
-                          used,
-                          row: i,
-                          col: j,
-                        });
-                      }}
-                      required
-                      onFocus={() => {
-                        onFocus(i, j);
-                      }}
-                    />
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <button
-            className={`rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20`}
-            onClick={() => {
-              mutate(triangle);
-            }}
-          >
-            Calculate
-          </button>
+      {numbers.map((row, i) => (
+        <div key={i} className={`flex gap-4`}>
+          {row.map(({ value, used, ref }, j) => (
+            <input
+              contentEditable={true}
+              type="number"
+              key={j}
+              ref={ref}
+              className={`resize-none overflow-hidden rounded-md ${
+                used ? "bg-pink-400" : "bg-white/10"
+              } h-12 w-12 text-center font-semibold text-white no-underline transition hover:bg-white/20`}
+              value={value}
+              onKeyDownCapture={(e) => {
+                keyInput({
+                  value: e.key === " " ? "Space" : e.key,
+                  row: i,
+                  col: j,
+                });
+              }}
+              onChange={(e) => {
+                updateValue({
+                  value: e.target.value,
+                  row: i,
+                  col: j,
+                });
+              }}
+              required
+              onFocus={() => {
+                onFocus(i, j);
+              }}
+            />
+          ))}
         </div>
-      </main>
+      ))}
     </>
   );
 };
--- packages/api/src/router/triangle.ts
+++ packages/api/src/router/triangle.ts
@@ -13,6 +13,96 @@ type Path = {
   cells: Cell[];
 };
 
+const findMaxPath = (
+  numbers: {
+    value: number;
+    row: number;
+    col: number;
+  }[][],
+): Path => {
+  const memo: Path[][] = [];
+
+  for (let row = numbers.length - 1; row >= 0; row--) {
+    memo[row] = [];
+    const rowLen = numbers[row]?.length;
+    if (!rowLen) continue;
+
+    for (let col = 0; col < rowLen; col++) {
+      const cell = numbers[row]?.[col];
+      if (!cell || !memo[row]) continue;
+
+      const nextRow = numbers[row + 1] || [];
+
+      if (nextRow.length === 0) {
+        if (!memo[row]) continue;
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        memo[row]![col] = { sum: cell.value, cells: [cell] };
+      } else {
+        const memoRow = memo[row + 1];
+        if (!memoRow) continue;
+
+        const leftPath = memoRow[col];
+        const rightPath = memoRow[col + 1];
+
+        if (!leftPath || !rightPath) continue;
+
+        if (leftPath.sum > rightPath.sum) {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          memo[row]![col] = {
+            sum: cell.value + leftPath.sum,
+            cells: [cell, ...leftPath.cells],
+          };
+        } else {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          memo[row]![col] = {
+            sum: cell.value + rightPath.sum,
+            cells: [cell, ...rightPath.cells],
+          };
+        }
+      }
+    }
+  }
+
+  return memo[0]?.[0] || { sum: 0, cells: [] };
+};
+
+const findMinPath = (
+  row: number,
+  col: number,
+  numbers: {
+    value: number;
+    row: number;
+    col: number;
+  }[][],
+): Path => {
+  const cell = numbers[row]?.[col];
+  const nextRow = numbers[row + 1] || [];
+
+  if (!cell) {
+    return { sum: 0, cells: [] };
+  }
+
+  if (nextRow.length === 0) {
+    return { sum: cell.value, cells: [cell] };
+  }
+
+  const leftPath = findMinPath(row + 1, col, numbers);
+  const rightPath = findMinPath(row + 1, col + 1, numbers);
+
+  if (leftPath.sum < rightPath.sum) {
+    return {
+      sum: cell.value + leftPath.sum,
+      cells: [cell, ...leftPath.cells],
+    };
+  } else {
+    return {
+      sum: cell.value + rightPath.sum,
+      cells: [cell, ...rightPath.cells],
+    };
+  }
+};
+
 export const triangleRouter = createTRPCRouter({
   maxValuePath: publicProcedure
     .input(z.string().min(1))
@@ -31,36 +121,28 @@ export const triangleRouter = createTRPCRouter({
         )
         .slice(0, -1);
 
-      const findMaxPath = (row: number, col: number): Path => {
-        const cell = numbers[row]?.[col];
-        const nextRow = numbers[row + 1] || [];
-
-        if (!cell) {
-          return { sum: 0, cells: [] };
-        }
-
-        if (nextRow.length === 0) {
-          return { sum: cell.value, cells: [cell] };
-        }
-
-        const leftPath = findMaxPath(row + 1, col);
-        const rightPath = findMaxPath(row + 1, col + 1);
-
-        if (leftPath.sum > rightPath.sum) {
-          return {
-            sum: cell.value + leftPath.sum,
-            cells: [cell, ...leftPath.cells],
-          };
-        } else {
-          return {
-            sum: cell.value + rightPath.sum,
-            cells: [cell, ...rightPath.cells],
-          };
-        }
-      };
-
-      const maxPath = findMaxPath(0, 0);
+      const maxPath = findMaxPath(numbers);
 
       return { sum: maxPath.sum, usedCells: maxPath.cells };
+    }),
+
+  minValuePath: publicProcedure
+    .input(z.string().min(1))
+    .mutation(({ input: triangle }) => {
+      const numbers = triangle.split("\n").map((row, i) =>
+        row
+          .trim()
+          .split(/\s+/)
+          .map((cell, col) => ({
+            value: +cell.replace(/\D/g, ""),
+            row: i,
+            col,
+          })),
+      );
+
+      console.log("\n\n\n", "a", "\n\n\n");
+      const minPath = findMinPath(0, 0, numbers);
+
+      return { sum: minPath.sum, usedCells: minPath.cells };
     }),
 });
